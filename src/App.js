57c95e790fa80bba2eca0ddcce0660d595509964
Natyctoc <brujadeluz@protonmail.com>
2022-02-27 16:16:34 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Galería digital de arte femenino</h1>
+        <container>
+        <p>
+          La historia del arte borró a la mujer de sus registros, hundiendo en el olvido la expresión de muchas mujeres interesantes que aportaron mucho al avance del conocimiento sensible y estético. Es hora de reivindicarlas, de traerlas al centro de la discusión sobre arte, haremos justicia y  les devolveremos a las grandes mujeres artista, la gloria que se merecen. 
+        </p>
+        </container> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
