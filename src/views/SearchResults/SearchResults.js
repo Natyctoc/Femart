c85361f6d646fbdd09e75921b268432f108fb6db
Natyctoc <brujadeluz@protonmail.com>
2022-02-27 18:22:44 -0500

    Inicio página de resultados de búsqueda

---
+++ src/views/SearchResults/SearchResults.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from 'react-bootstrap/Button'
+
+function Searches() {
+    return (
+        <div className="Searches">
+            <header>
+                <h1>
+                    Resultados de búsqueda.
+                </h1>
+                <Button>Hacer otra búsqueda</Button>
+            </header>
+        </div>
+    )
+}
+
+export default Searches;
